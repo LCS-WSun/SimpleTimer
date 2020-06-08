03432eb020abb2fcf10dfa3ee5c9f4fdd69bc95f
Wesley Sun <wsun@lcs.on.ca>
2020-06-07 20:48:27 -0400

    got child view added

---
+++ SimpleTimer/ChildViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChildViewController.swift
+//  SimpleTimer
+//
+//  Created by Sun, Wesley on 2020-06-07.
+//  Copyright © 2020 Wesley Sun. All rights reserved.
+//
+
+import UIKit
+
+class ChildViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
