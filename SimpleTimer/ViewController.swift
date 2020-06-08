8571ebb48274b1da457822796b16a899ded78516
Wesley Sun <wsun@lcs.on.ca>
2020-06-07 20:42:45 -0400

    Initial Commit

---
+++ SimpleTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTimer
+//
+//  Created by Sun, Wesley on 2020-06-07.
+//  Copyright © 2020 Wesley Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
